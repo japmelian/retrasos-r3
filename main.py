8781c978138a8d34f21f1446570677931309f18c
Jose Alberto <japmelian@gmail.com>
2025-01-27 01:48:33 +0100

    Datos añadidos del 26 de enero

--- main.py
+++ main.py
@@ -118,7 +118,9 @@ with st.container():
                         unsafe_allow_html=True
                         )
     
-
+    st.write('')
+    st.error('**Importante:** Debido a la escasa información proporcionada desde los orígenes de datos es imposible determinar qué trenes o circulaciones han resultado canceladas.')
+    st.info('**Nota:** Las paradas han sido renombradas para una mejor visualización de los datos.')
     st.divider()
 
 
@@ -133,62 +135,111 @@ for file in os.listdir('data'):
 all_dates = set(all_dates)
 
 with st.container():
-    col1, col2 = st.columns([0.5, 3])
+    col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        opcion_fecha = None
-        opcion_sentido = None
-
-        # Crear un desplegable con opciones
-        opcion_fecha = st.selectbox(
-            'Selecciona un día',  # Título del desplegable
-            [''] + sorted(all_dates)  # Opciones disponibles
-        )
-
-        st.write('')
-
-        if opcion_fecha:
-            opcion_sentido = st.selectbox(
-                'Selecciona el sentido',  # Título del desplegable
-                [''] + all_ways,  # Opciones disponibles
-            )
-
-        if opcion_fecha and opcion_sentido:
-            _opcion_fecha = str(opcion_fecha).replace('-', '')
-            _opcion_sentido = None
-
-            if 'Ida' in opcion_sentido:
-                _opcion_sentido = 'ida'
-            else:
-                _opcion_sentido = 'vuelta'
-
-            data = pd.read_csv(f'data/{_opcion_fecha}-R3-{_opcion_sentido}.csv')
-            data['Tren'] = data['Tren'].astype(str)
-
-            def color_celdas(val):
-                if isinstance(val, (int, float)):  # Verificar si el valor es numérico
-                    if val <= 3:
-                        color = '#6AA84F'
-                    elif 4 <= val <= 7:
-                        color = '#F1C232'
-                    elif 8 <= val <= 14:
-                        color = '#E69138'
-                    elif val >= 15:
-                        color = '#CC0000'
-                    else:
-                        color = ''
-                    return f'background-color: {color}'
-                else:
-                    return ''  # No aplicar color si no es un valor numérico
-                    
-            # Formatear solo las columnas numéricas sin decimales
-            styled_df = data.style.map(color_celdas)
-
-            # Aplicar el formato para las columnas numéricas sin decimales
-            styled_df = styled_df.format({col: "{:.0f}" for col in data.select_dtypes(include=['number']).columns})          
+        st.markdown('## Visualiza los datos')
+        st.markdown('Selecciona un día y un sentido')
 
     with col2:
-        st.info('Puedes usar las barras laterales y verticales para moverte por la tabla')
+        col2_1, col2_2 = st.columns(2)
 
-        if 'styled_df' in locals():  # Verificar que df ha sido generado
-            st.dataframe(styled_df, hide_index = True, height=800)
+        with col2_1:
+            opcion_fecha = None
+            opcion_sentido = None
+
+            # Crear un desplegable con opciones
+            opcion_fecha = st.selectbox(
+                'Selecciona un día',  # Título del desplegable
+                [''] + sorted(all_dates)  # Opciones disponibles
+            )
+
+        with col2_2:
+            if opcion_fecha:
+                opcion_sentido = st.selectbox(
+                    'Selecciona el sentido',  # Título del desplegable
+                    [''] + all_ways,  # Opciones disponibles
+                )
+
+            if opcion_fecha and opcion_sentido:
+                _opcion_fecha = str(opcion_fecha).replace('-', '')
+                _opcion_sentido = None
+
+                if 'Ida' in opcion_sentido:
+                    _opcion_sentido = 'ida'
+                else:
+                    _opcion_sentido = 'vuelta'
+
+                data = pd.read_csv(f'data/{_opcion_fecha}-R3-{_opcion_sentido}.csv')
+                data['Tren'] = data['Tren'].astype(str)
+
+                
+
+                def color_celdas(val):
+                    if isinstance(val, (int, float)):  # Verificar si el valor es numérico
+                        if val <= 3:
+                            color = '#6AA84F'
+                        elif 4 <= val <= 7:
+                            color = '#F1C232'
+                        elif 8 <= val <= 14:
+                            color = '#E69138'
+                        elif val >= 15:
+                            color = '#CC0000'
+                        else:
+                            color = ''
+                        return f'background-color: {color}'
+                    else:
+                        return ''  # No aplicar color si no es un valor numérico
+                        
+                # Formatear solo las columnas numéricas sin decimales
+                styled_df = data.style.map(color_celdas)
+
+                # Aplicar el formato para las columnas numéricas sin decimales
+                styled_df = styled_df.format({col: "{:.0f}" for col in data.select_dtypes(include=['number']).columns})
+
+    with col3:
+        col3_1, col3_2, col3_3 = st.columns([1, 2, 1])
+
+        with col3_1:
+            a = 1
+        
+        with col3_2:
+            if 'data' in locals():
+                data_csv = data.to_csv(index=False).encode("utf-8")
+
+                st.download_button(
+                    label="Descargar datos",
+                    data=data_csv,
+                    file_name=f'{_opcion_fecha}-R3-{_opcion_sentido}.csv',
+                    mime="text/csv"
+                )
+
+        with col3_3:
+            a = 1
+    with col4:
+        col4_1, col4_2, col4_3 = st.columns([1, 2, 1])
+
+        with col4_1:
+            a = 1
+        
+        with col4_2:
+            if 'data' in locals():
+                correspondencia = pd.read_csv('data/acronimos.csv', sep = ',')
+                correspondencia_csv = correspondencia.to_csv(index = False)
+
+                st.download_button(
+                    label="Descargar paradas",
+                    data=correspondencia_csv,
+                    file_name = 'correspondencia_paradas.csv',
+                    mime="text/csv"
+                )
+
+        with col4_3:
+            a = 1
+        
+
+st.write('')
+
+with st.container():
+    if 'styled_df' in locals():  # Verificar que df ha sido generado
+        st.dataframe(styled_df, hide_index = True, height=800)
